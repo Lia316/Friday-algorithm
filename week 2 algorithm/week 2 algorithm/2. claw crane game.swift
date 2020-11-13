249733ce76e999a130643571b393b88c2d1631ec
Lia <judykim257@gmail.com>
2020-11-13 16:17:41 +0900

    2. 인형뽑기 크레인 게임 주석 추가

--- week 2 algorithm/week 2 algorithm/2. claw crane game.swift
+++ week 2 algorithm/week 2 algorithm/2. claw crane game.swift
@@ -9,18 +9,18 @@ import Foundation
 
 func solution2(_ board:[[Int]], _ moves:[Int]) -> Int {
     var boardArr = board
-    var basketArr = [-1, 0]
-    var countValidMove = 0
+    var basketArr = [-1, 0]     // 예외 처리 안 하기 위함
+    var countValidMove = 0      // 유효한 move 만 측정하여 나중에 삭제 인형 수 계산
     
     for moveIndex in moves {
-        for boardIndex in 0..<board.count{
+        for boardIndex in 0..<board.count{ // 0 이 아니면 값을 basket에 저장 후, 지움
             if(boardArr[boardIndex][moves[moveIndex] - 1] != 0){
                 basketArr.append(boardArr[boardIndex][moves[moveIndex] - 1])
                 boardArr[boardIndex][moves[moveIndex] - 1] = 0
                 countValidMove += 1
                 break
             }
-        }
+        }// 겹치면 삭제
         if(basketArr.last == basketArr[basketArr.endIndex - 2]){
             basketArr.popLast()
             basketArr.popLast()
---
+++ week 2 algorithm/week 2 algorithm/3. select & add array.swift
@@ -0,0 +1,8 @@
+//
+//  3. select & add array.swift
+//  week 2 algorithm
+//
+//  Created by Lia on 2020/11/13.
+//
+
+import Foundation
