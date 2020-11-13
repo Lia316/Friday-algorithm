25cdc5308c07dd522ca5c9a16d01ac527efa5490
Lia <judykim257@gmail.com>
2020-11-13 20:22:05 +0900

    4. 수포자의 모의고사 - 다른 사람 풀이 보고 공부한 뒤 추가 작성

--- week 2 algorithm/week 2 algorithm/4. model test score.swift
+++ week 2 algorithm/week 2 algorithm/4. model test score.swift
@@ -37,3 +37,22 @@ func solution4(_ answers:[Int]) -> [Int] {
     
     return result
 }
+
+
+// 다른 사람 코드 보고 공부하며 적음
+func otherSolution4(_ answers:[Int]) -> [Int] {
+    let student = (
+        a: [1, 2, 3, 4, 5],
+        b: [2, 1, 2, 3, 2, 4, 2, 5],
+        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    )
+    var score : [Int:Int] = [1:0, 2:0, 3:0]
+    
+    for index in 0..<answers.count{
+        if(answers[index % 5] == student.a[index]) {score[1]! += 1}
+        if(answers[index % 8] == student.b[index]) {score[2]! += 1}
+        if(answers[index % 10] == student.c[index]) {score[3]! += 1}
+    }
+    
+    return score.filter{ $0.1 == score.values.max()}.map{ $0.0 }.sorted()
+}
