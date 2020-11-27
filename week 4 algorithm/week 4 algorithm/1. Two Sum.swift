40ebb1f72bce28f49cca1ca9c4a10cbe7a5644ed
Lia <judykim257@gmail.com>
2020-11-27 14:23:32 +0900

    .

--- week 4 algorithm/week 4 algorithm/1. Two Sum.swift
+++ week 4 algorithm/week 4 algorithm/1. Two Sum.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Solution {
+class Solution1 {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var answerArr = [Int]()
         
--- week 4 algorithm/week 4 algorithm/main.swift
+++ week 4 algorithm/week 4 algorithm/main.swift
@@ -7,17 +7,21 @@
 
 import Foundation
 
+// *************** 1. Two Sum  ******************
 
-let input1 = [2,7,11,15]
-let target1 = 9
+//let input1 = [2,7,11,15]
+//let target1 = 9
+//
+//let input2 = [3,2,4]
+//let target2 = 6
+//
+//let input3 = [3,3]
+//let target3 = 6
+//
+//let test1 = Solution1()
+//print(test1.twoSum(input1, target1))
+//print(test1.twoSum(input2, target2))
+//print(test1.twoSum(input3, target3))
 
-let input2 = [3,2,4]
-let target2 = 6
+// *************** 2. Reverse Intege ******************
 
-let input3 = [3,3]
-let target3 = 6
-
-let test1 = Solution()
-print(test1.twoSum(input1, target1))
-print(test1.twoSum(input2, target2))
-print(test1.twoSum(input3, target3))
